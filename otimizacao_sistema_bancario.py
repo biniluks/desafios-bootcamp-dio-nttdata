abf569ace4bda37470f84054d04a6fb389b6b7cf
biniluks <r.santoosp@gmail.com>
2024-09-12 10:30:25 -0300

    inicio de código

---
+++ otimizacao_sistema_bancario.py
@@ -0,0 +1,14 @@
+def menu():
+    menu='''
+    Bem-vindo ao seu banco!
+    Selecione a opção desejada:
+
+    [1] - Depósito
+    [2] - Saque
+    [3] - Extrato
+    [4] - Abrir conta
+    
+    [0] - Sair
+
+    '''
+    return(menu)
